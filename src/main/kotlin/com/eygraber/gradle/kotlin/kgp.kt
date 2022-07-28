e8f8b3b76464b3241225c371c9c6c21fb267556e
Eliezer Graber <me@eygraber.com>
2022-07-28 19:39:27 -0400

    Allow jdk version to be specified as a Provider<String>

--- src/main/kotlin/com/eygraber/gradle/kotlin/kgp.kt
+++ src/main/kotlin/com/eygraber/gradle/kotlin/kgp.kt
@@ -1,12 +1,27 @@
 package com.eygraber.gradle.kotlin
 
 import org.gradle.api.Project
+import org.gradle.api.provider.Provider
 import org.gradle.jvm.toolchain.JavaLanguageVersion
 import org.gradle.jvm.toolchain.JvmVendorSpec
 import org.jetbrains.kotlin.gradle.dsl.ExplicitApiMode
 import org.jetbrains.kotlin.gradle.dsl.KotlinProjectExtension
 import org.jetbrains.kotlin.gradle.plugin.KotlinBasePluginWrapper
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+public fun Project.setupKgp(
+  jdkVersion: Provider<String>,
+  allWarningsAsErrors: Boolean = true,
+  explicitApiMode: ExplicitApiMode = ExplicitApiMode.Disabled,
+  vararg optIns: String
+) {
+  setupKgp(
+    jdkVersion.get(),
+    allWarningsAsErrors,
+    explicitApiMode,
+    *optIns
+  )
+}
 
 public fun Project.setupKgp(
   jdkVersion: String,
