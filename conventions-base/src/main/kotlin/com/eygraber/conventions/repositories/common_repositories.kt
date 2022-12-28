bd2ddb70d8cd0304fe274f2772b30bd5222880ba
Eliezer Graber <me@eygraber.com>
2022-12-28 07:58:06 -0500

    Add a function for adding common repositories (#107)

---
+++ conventions-base/src/main/kotlin/com/eygraber/conventions/repositories/common_repositories.kt
@@ -0,0 +1,64 @@
+package com.eygraber.conventions.repositories
+
+import org.gradle.api.Action
+import org.gradle.api.artifacts.dsl.RepositoryHandler
+import org.gradle.api.artifacts.repositories.ArtifactRepository
+import org.gradle.api.artifacts.repositories.MavenArtifactRepository
+import org.gradle.internal.Actions
+import org.gradle.kotlin.dsl.maven
+
+public fun RepositoryHandler.addCommonRepositories(
+  includeMavenCentral: Boolean = true,
+  mavenCentral: Action<MavenArtifactRepository> = Actions.doNothing(),
+  includeMavenCentralSnapshots: Boolean = false,
+  mavenCentralSnapshots: Action<MavenArtifactRepository> = Actions.doNothing(),
+  includeGoogle: Boolean = false,
+  google: Action<MavenArtifactRepository> = Actions.doNothing(),
+  includeJetbrainsCompose: Boolean = false,
+  jetbrainsCompose: Action<MavenArtifactRepository> = Actions.doNothing(),
+  includeJitpack: Boolean = false,
+  jitpack: Action<MavenArtifactRepository> = Actions.doNothing(),
+  includeGradlePluginPortal: Boolean = false,
+  gradlePluginPortal: Action<ArtifactRepository> = Actions.doNothing()
+) {
+  if(includeGoogle) {
+    google {
+      content {
+        includeGroupByRegex("com\\.google.*")
+        includeGroupByRegex("com\\.android.*")
+        includeGroupByRegex("androidx.*")
+      }
+
+      google.execute(this)
+    }
+  }
+
+  if(includeJetbrainsCompose) {
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev") {
+      content {
+        includeGroupByRegex("org\\.jetbrains.*")
+      }
+
+      jetbrainsCompose.execute(this)
+    }
+  }
+
+  if(includeMavenCentral) {
+    mavenCentral(mavenCentral)
+  }
+
+  if(includeMavenCentralSnapshots) {
+    mavenCentralSnapshots(mavenCentralSnapshots)
+    mavenCentralSnapshotsS01(mavenCentralSnapshots)
+  }
+
+  if(includeJitpack) {
+    maven("https://jitpack.io") {
+      jitpack.execute(this)
+    }
+  }
+
+  if(includeGradlePluginPortal) {
+    gradlePluginPortal(gradlePluginPortal)
+  }
+}
