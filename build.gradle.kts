36c8a74cd67c2a3322cb858af15341d492b9163c
Eliezer Graber <me@eygraber.com>
2022-12-28 12:47:47 -0500

    Use the new convention plugins instead of buildSrc

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+import com.eygraber.conventions.kotlin.KotlinOptIn
+
+buildscript {
+  dependencies {
+    classpath(libs.buildscript.detekt)
+    classpath(libs.buildscript.dokka)
+    classpath(libs.buildscript.kotlin)
+    classpath(libs.buildscript.publish)
+  }
+}
+
+plugins {
+  alias(libs.plugins.gradleConventions)
+}
+
+gradleConventionsDefaults {
+  detekt {
+    plugins(
+      libs.detekt,
+      libs.detektEygraber.formatting,
+      libs.detektEygraber.style
+    )
+  }
+  kotlin {
+    jdkVersion = libs.versions.jdk.get()
+    jvmDistribution = JvmVendorSpec.AZUL
+    allWarningsAsErrors = true
+    optIns = setOf(KotlinOptIn.RequiresOptIn)
+  }
+}
