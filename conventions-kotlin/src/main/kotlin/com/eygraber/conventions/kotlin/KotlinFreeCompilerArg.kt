a64b1bedd881bfd66ef7948609ce1bc099c08d16
Eliezer Graber <me@eygraber.com>
2024-01-17 22:21:33 -0500

    Add a FreeCompilerArg for allowing expect/actual classes (#312)

--- conventions-kotlin/src/main/kotlin/com/eygraber/conventions/kotlin/KotlinFreeCompilerArg.kt
+++ conventions-kotlin/src/main/kotlin/com/eygraber/conventions/kotlin/KotlinFreeCompilerArg.kt
@@ -1,5 +1,6 @@
 package com.eygraber.conventions.kotlin
 
 public sealed class KotlinFreeCompilerArg(public val value: String) {
+  public object AllowExpectActualClasses : KotlinFreeCompilerArg("-Xexpect-actual-classes")
   public class Unknown(value: String) : KotlinFreeCompilerArg(value)
 }
