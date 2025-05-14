eb7d89dd5122bd0e92e72508fb58757b04fa5deb
Eliezer Graber <me@eygraber.com>
2025-05-14 20:29:00 +0000

    Allow setting the publishing host (#461)

--- conventions-plugin/src/main/kotlin/com.eygraber.conventions-publish-maven-central.gradle.kts
+++ conventions-plugin/src/main/kotlin/com.eygraber.conventions-publish-maven-central.gradle.kts
@@ -1,11 +1,18 @@
-import com.vanniktech.maven.publish.SonatypeHost
+import com.eygraber.conventions.gradleConventionsDefaultsService
+import com.eygraber.conventions.gradleConventionsExtension
 
 plugins {
   id("org.jetbrains.dokka")
   id("com.vanniktech.maven.publish")
 }
 
-mavenPublishing {
-  publishToMavenCentral(SonatypeHost.S01, automaticRelease = true)
-  signAllPublications()
+val ext = gradleConventionsExtension
+val publishDefaults = gradleConventionsDefaultsService.publish
+ext.publish.host = publishDefaults.host
+
+ext.awaitPublishConfigured {
+  mavenPublishing {
+    publishToMavenCentral(host, automaticRelease = true)
+    signAllPublications()
+  }
 }
