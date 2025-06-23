61af28d71d661162b1fc1582ce8a8a60ce871ede
Eliezer Graber <me@eygraber.com>
2025-06-23 15:18:19 -0400

    Add helper for specifying Kotlin warning levels

--- conventions-kotlin/src/main/kotlin/com/eygraber/conventions/kotlin/KotlinFreeCompilerArg.kt
+++ conventions-kotlin/src/main/kotlin/com/eygraber/conventions/kotlin/KotlinFreeCompilerArg.kt
@@ -4,3 +4,14 @@ public sealed class KotlinFreeCompilerArg(public val value: String) {
   public object AllowExpectActualClasses : KotlinFreeCompilerArg("-Xexpect-actual-classes")
   public class Unknown(value: String) : KotlinFreeCompilerArg(value)
 }
+
+public enum class WarningLevel(val value: String) {
+  Disabled("disabled"),
+  Warning("warning"),
+  Error("error"),
+}
+
+public data class KotlinWarningLevel(
+  public val diagnosticName: String,
+  public val level: WarningLevel,
+) : KotlinFreeCompilerArg("-Xwarning-level=$diagnosticName:${level.value}")
