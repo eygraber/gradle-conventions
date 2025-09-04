741345a41e2db2845cd097cb6e6e3e6653d6d8cb
Eliezer Graber <me@eygraber.com>
2025-09-04 15:31:30 -0400

    WIP: detekt 2

--- conventions-plugin/src/main/kotlin/com.eygraber.conventions-detekt.gradle.kts
+++ conventions-plugin/src/main/kotlin/com.eygraber.conventions-detekt.gradle.kts
@@ -2,14 +2,21 @@ import com.eygraber.conventions.detekt.configureDetekt
 import com.eygraber.conventions.detekt.registerCommonTestDetektTask
 import com.eygraber.conventions.gradleConventionsDefaultsService
 import com.eygraber.conventions.gradleConventionsExtension
+import dev.detekt.gradle.extensions.DetektExtension
 
 plugins {
-  id("io.gitlab.arturbosch.detekt")
+  id("dev.detekt")
 }
 
 val ext = gradleConventionsExtension
 val detektDefaults = gradleConventionsDefaultsService.detekt
 val kotlinDefaults = gradleConventionsDefaultsService.kotlin
+
+ext.detekt.autoCorrect = detektDefaults.autoCorrect
+ext.detekt.parallel = detektDefaults.parallel
+ext.detekt.debug = detektDefaults.debug
+ext.detekt.enableCompilerPlugin = detektDefaults.enableCompilerPlugin
+ext.detekt.buildUponDefaultConfig = detektDefaults.buildUponDefaultConfig
 
 ext.detekt.ignoredAndroidFlavors = detektDefaults.ignoredAndroidFlavors
 ext.detekt.ignoredAndroidVariants = detektDefaults.ignoredAndroidVariants
@@ -24,6 +31,14 @@ ext.awaitKotlinConfigured { isKotlinUserConfigured ->
     configureDetekt(
       jvmTargetVersion = jvmTargetVersion,
     )
+
+    extensions.configure<DetektExtension>("detekt") {
+      this.autoCorrect.set(ext.detekt.autoCorrect)
+      this.parallel.set(ext.detekt.parallel)
+      this.debug.set(ext.detekt.debug)
+      this.enableCompilerPlugin.set(ext.detekt.enableCompilerPlugin)
+      this.buildUponDefaultConfig.set(ext.detekt.buildUponDefaultConfig)
+    }
 
     dependencies {
       for(dependency in detektPluginDependencies) {
