b587c50e0e265c6da3ab0211a71bc44a83cd1429
Eliezer Graber <me@eygraber.com>
2025-10-15 23:15:58 -0400

    Support Java 25 starting from Gradle 9.1.0

--- conventions-kotlin/src/main/kotlin/com/eygraber/conventions/kotlin/kgp.kt
+++ conventions-kotlin/src/main/kotlin/com/eygraber/conventions/kotlin/kgp.kt
@@ -63,6 +63,7 @@ public fun Project.configureKgp(
   val lowestSupportedJava = JavaVersion.VERSION_17
   val gradleVersion = GradleVersion.current()
   val highestSupportedJava: JavaVersion = when {
+    gradleVersion >= GradleVersion.version("9.1.0") -> JavaVersion.VERSION_25
     gradleVersion >= GradleVersion.version("8.14") -> JavaVersion.VERSION_24
     gradleVersion >= GradleVersion.version("8.10") -> JavaVersion.VERSION_23
     gradleVersion >= GradleVersion.version("8.8") -> JavaVersion.VERSION_22
